26418ccae7aec7bdaea1b3c7ee9be7e1d2d86f07
Manoj Neupane <manojonurnet@gmail.com>
2015-04-25 11:23:56 +0545

    Initial Commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/jagatradeviStore/admin/');
+define('HTTP_CATALOG', 'http://localhost:81/jagatradeviStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/jagatradeviStore/admin/');
+define('HTTPS_CATALOG', 'http://localhost:81/jagatradeviStore/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\jagatradeviStore/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\jagatradeviStore/system/');
+define('DIR_DATABASE', 'C:\wamp\www\jagatradeviStore/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\jagatradeviStore/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\jagatradeviStore/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\jagatradeviStore/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\jagatradeviStore/image/');
+define('DIR_CACHE', 'C:\wamp\www\jagatradeviStore/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\jagatradeviStore/download/');
+define('DIR_LOGS', 'C:\wamp\www\jagatradeviStore/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\jagatradeviStore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'jagatradeviStore');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/jagatradeviStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/jagatradeviStore/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\jagatradeviStore/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\jagatradeviStore/system/');
+define('DIR_DATABASE', 'C:\wamp\www\jagatradeviStore/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\jagatradeviStore/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\jagatradeviStore/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\jagatradeviStore/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\jagatradeviStore/image/');
+define('DIR_CACHE', 'C:\wamp\www\jagatradeviStore/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\jagatradeviStore/download/');
+define('DIR_LOGS', 'C:\wamp\www\jagatradeviStore/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'jagatradeviStore');
+define('DB_PREFIX', 'oc_');
+?>
